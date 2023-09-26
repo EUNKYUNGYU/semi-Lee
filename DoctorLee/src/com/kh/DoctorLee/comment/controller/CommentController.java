93c7d7e8de4e255f850df7d3c54f7611207ecd69
aadamk613 <aadamk@naver.com>
2023-09-26 17:46:16 +0900

    패키지 만들었어요~

---
+++ DoctorLee/src/com/kh/DoctorLee/comment/controller/CommentController.java
@@ -0,0 +1,5 @@
+package com.kh.DoctorLee.comment.controller;
+
+public class CommentController {
+
+}
---
+++ DoctorLee/src/com/kh/DoctorLee/quize/model/vo/Quize.java
@@ -0,0 +1,17 @@
+package com.kh.DoctorLee.quize.model.vo;
+
+public class Quize {
+
+	private int quizeNo;
+	private String quizeTitle;
+	private String quizeContent;
+	private String createDate;
+	private String endDate;
+	private int vote;
+	private String choice1;
+	private String choice2;
+	private String choice3;
+	private String choice4;
+	private int answer;
+	
+}

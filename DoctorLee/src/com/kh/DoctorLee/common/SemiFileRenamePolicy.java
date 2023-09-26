e117c980d497fcd24ddecb3df81b69e3c659b8f3
doyeon0115 <kwondy1892@gmail.com>
2023-09-26 17:22:22 +0900

    common - 권도연
    페이징 처리 vo, 파일수정명 컨트롤러

---
+++ DoctorLee/src/com/kh/DoctorLee/common/SemiFileRenamePolicy.java
@@ -0,0 +1,31 @@
+package com.kh.DoctorLee.common;
+
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
+public class SemiFileRenamePolicy implements FileRenamePolicy {
+
+	@Override
+	public File rename(File originFile) {
+		
+		String originName = originFile.getName();
+		
+		String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		
+		int randomNum = (int)(Math.random() * 90000) + 10000;
+		
+		String ext = originName.substring(originName.lastIndexOf("."));
+		
+		String changeName = "KH_" + currentTime + "_" + randomNum + ext;
+		
+		return new File(originFile.getParent(), changeName);
+	}
+	
+	
+
+}
+

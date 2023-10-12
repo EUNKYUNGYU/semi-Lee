75a6e415060868c0f414b267b78cd7d777b4c3dd
hyunzae <146050667+hyunzae@users.noreply.github.com>
2023-10-13 00:38:27 +0900

    복약관리 테이블 다시

---
+++ DoctorLee/src/com/kh/DoctorLee/mpBoard/controller/MedManagementDetailController.java
@@ -0,0 +1,44 @@
+package com.kh.DoctorLee.mpBoard;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MedManagementDetailController
+ */
+@WebServlet("/detil.medi")
+public class MedManagementDetailController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MedManagementDetailController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		request.getRequestDispatcher("views/myPage/medManagementDetail.jsp").forward(request, response);
+		
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

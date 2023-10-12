fbbf00fe298c5e7c6c4fea2381231429a658bb34
aadamk613 <aadamk@naver.com>
2023-10-12 09:22:25 +0900

    MessageDetailController하는 중

---
+++ DoctorLee/src/com/kh/DoctorLee/message/controller/MessageDetailController.java
@@ -0,0 +1,47 @@
+package com.kh.DoctorLee.message.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MessageDetailController
+ */
+@WebServlet("/detail.ms")
+public class MessageDetailController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MessageDetailController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		
+		request.getParameter("");
+		request.getParameter("");
+		request.getParameter("");
+		request.getParameter("");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

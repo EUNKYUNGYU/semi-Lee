8da4f47f5b43f324f5a813075b4eb55c84b78105
hyunzae <146050667+hyunzae@users.noreply.github.com>
2023-10-19 15:45:43 +0900

    js 뽑기

---
+++ DoctorLee/WebContent/resources/js/member/loginForm.js
@@ -0,0 +1,11 @@
+ 		function enrollPage(){
+            location.href = "<%=contextPath%>/enrollForm.me";
+        }
+        
+        function findIdPage(){
+            location.href = "<%=contextPath%>/findIdForm.me";
+        }
+        
+        function findPwdPage(){
+            location.href = "<%=contextPath%>/findPwdForm.me";
+        }
